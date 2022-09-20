623d3dd5cdb90ce77b61dda883a7e1212c4019a3
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-20 12:39:07 +0100

    Add #complete test for multiple complete

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -30,6 +30,17 @@ RSpec.describe 'TodoList integration' do
       todo2.mark_done!
       expect(todo_list.complete).to eq 'Complete phase 2 challenge 9'
     end
+
+    it 'Returns multiple complete todos' do
+      todo_list = TodoList.new
+      todo = Todo.new('Complete phase 2 challenge 8')
+      todo2 = Todo.new('Complete phase 2 challenge 9')
+      todo_list.add todo
+      todo_list.add todo2
+      todo.mark_done!
+      todo2.mark_done!
+      expect(todo_list.complete).to eq 'Complete phase 2 challenge 8, Complete phase 2 challenge 9'
+    end
   end
 
   describe '#incomplete' do
